44558cf64a1f0b9800347a62e24e1697df8eb769
Micah R Ledbetter <mrled@younix.us>
2015-05-17 17:28:26 -0500

    Clean up JS, add stuff to TODO

--- readme.markdown
+++ readme.markdown
@@ -53,3 +53,7 @@ Furthermore, this can solve the subdirectory problem:
 - Select the shortest verse for the `og:description` so you're most likely to see the whole thing when shared to FB etc
 - Should use a Mako page filter, rather than filtering on each expression
 - Should filter on input side, in bmweb.py, in addition to output side, in index.mako. 
+- Reverse the sorting of the recent searches
+- Fix the bug where recent searches appear more than once
+- More consistent layout would be nice, CSS is hard
+
--- static/bmweb.js
+++ static/bmweb.js
@@ -20,36 +20,6 @@ function toggleHideId(Id) {
         elem.style.display = "";
     }
 }
-/*
-function toggleHideWtf() {
-    elem = document.getElementById("wtfSection");
-    if (elem.style.display == "") {
-        elem.style.display = "none";
-    }
-    else {
-        elem.style.display = "";
-    }
-}
-function toggleHideFavorites() {
-    elem = document.getElementById("searchFavorites");
-    console.log(elem.style.display)
-    if (elem.style.display == "") {
-        elem.style.display = "none";
-    }
-    else {
-        elem.style.display = "";
-    }
-}
-function toggleHideRecents() {
-    elem = document.getElementById("searchRecents");
-    if (elem.style.display == "") {
-        elem.style.display = "none";
-    }
-    else {
-        elem.style.display = "";
-    }
-}
-*/
 window.onload = function() { 
     emboldenMunged();  
 };
